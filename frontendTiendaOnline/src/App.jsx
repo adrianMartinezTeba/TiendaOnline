3e6a40eafa27660cd508c3da021d469e9f07801f
adrianMartinezTeba <adrymate@gmail.com>
2023-05-15 12:40:46 +0200

    añadido router y componente home

--- frontendTiendaOnline/src/App.jsx
+++ frontendTiendaOnline/src/App.jsx
@@ -2,18 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import { ProductsProvider } from "./context/ProductsContext/ProductsState";
 import Products from "./components/Products/Products";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <>
-      {/* <BrowserRouter> */}
-    <ProductsProvider>
-              {/* <Routes>
-                <Route path="/products" element={<Products/>} />
-              </Routes> */}
-              <Products/>
-              </ProductsProvider>
-      {/* </BrowserRouter> */}
+      <BrowserRouter>
+      <ProductsProvider>
+        <Routes>
+          <Route path="/products" element={<Products/>} />
+          <Route path="/" element={<Home/>}/>
+ 
+        </Routes>
+      </ProductsProvider>
+      </BrowserRouter>
     </>
   );
 }
---
+++ frontendTiendaOnline/src/components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.scss";
+import { Link, Navigate } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Bienvenidos a Gamer's Paradise</h1>
+      <p>
+        Aquí encontrarás todo lo que necesitas para hacer tus compras desde la
+        comodidad de tu hogar. Tenemos una gran variedad de productos a precios
+        competitivos y con envío a todo el mundo.
+      </p>
+      <Link to={'/products'}>
+      <button>Comenzar a Comprar</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
